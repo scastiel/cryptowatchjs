53b67dee4bf3a9b615215596a91ded5cc3f4c864
Sébastien Castiel <sebastien@castiel.me>
2017-12-12 20:41:01 -0500

    Use existing implementation of fetch if possible

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,20 @@
 // @flow
 
-const fetch = require('isomorphic-fetch')
+function getFetch () {
+  /* global fetch */
+  try {
+    if (fetch) {
+      return fetch
+    }
+  } catch (err) {}
+  try {
+    return require('isomorphic-fetch')
+  } catch (err) {}
+  throw new Error('Cannot find an implementation of `fetch`.')
+}
 
 async function returnResultFromUrl (url: string): Promise<any> {
+  const fetch = getFetch()
   const res = await fetch(url)
   const json = await res.json()
   return json.result
